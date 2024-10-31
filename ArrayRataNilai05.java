4919e96d8fa7a2747e551345e2e3586edd7538e1
BintangPancahaya <bintang.panca007@gmail.com>
2024-10-31 09:11:33 +0700

    New Changes

--- ArrayRataNilai05.java
+++ ArrayRataNilai05.java
@@ -6,16 +6,25 @@ public class ArrayRataNilai05 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int lulus = 0;
+        int mhs;
+        
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        mhs = input05.nextInt();
 
-        for (int i=0;i<nilaiMhs.length;i++) {
+        for (int i=0;i<mhs;i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = input05.nextInt();
         }
         for (int i=0;i<nilaiMhs.length;i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                lulus++;
+            }
         }
-        rata2 = total/nilaiMhs.length;
+        rata2 = total/mhs;
         System.out.println("Rata-rata nilai = "+rata2);
+        System.out.println("Mahasiswa yang lulus ada "+lulus);
 
         input05.close();
     }
