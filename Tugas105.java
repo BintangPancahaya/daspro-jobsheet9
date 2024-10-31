24c5609ea2707aad296409de562dacdfab4fec21
BintangPancahaya <bintang.panca007@gmail.com>
2024-10-31 10:22:34 +0700

    Tugas

--- Tugas105.java
+++ Tugas105.java
@@ -3,20 +3,33 @@ public class Tugas105 {
  public static void main(String[] args) {
     Scanner input05 = new Scanner(System.in);
 
-    System.out.print("Masukkan jumlah mahasiswa: ");
+    System.out.print("Masukkan banyaknya mahasiswa yang diinput: ");
     int mhs = input05.nextInt();
 
-    double total = 0;
+    double total = 0, tertinggi = 0, terendah = 100;
+    double nilaiMhs[] = new double[mhs];
 
-    for (int i=1;i<=mhs;i++) {
-        double nilaiMhs[] = new double[i];
-        System.out.print("Masukkan nilai mahasiswa ke-"+i+" : ");
+    for (int i=0;i<mhs;i++) {
+        System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
         nilaiMhs[i] = input05.nextDouble();
-    
-    for (int j=0;i<nilaiMhs.length;j++) {
-        total += nilaiMhs[j];
-     }
-     double rata2 = total/nilaiMhs.length;
     }
+    for (int i=0;i<mhs;i++) {
+        total += nilaiMhs[i];
+    }
+    double rata2 = total/mhs;
+    System.out.println("Rata-rata nilai mahasiswa adalah "+rata2);
+    for (int i=0;i<mhs;i++) {
+        if (nilaiMhs[i] > tertinggi) {
+            tertinggi = nilaiMhs[i];
+        } else if (nilaiMhs[i] < terendah) {
+            terendah = nilaiMhs[i];
+        }
+    }
+    System.out.println("Nilai mahasiswa yang tertinggi "+tertinggi);
+    System.out.println("Nilai mahasiswa yang terendah "+terendah);
+    for (int i=0;i<mhs;i++) {
+    System.out.println("Nilai mahasiswa ke-"+(i+1)+" adalah "+nilaiMhs[i]);
+    }
+    input05.close();
  }   
 }
