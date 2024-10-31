50f4f726e03ed5bd7e0287a43405b95ebb632864
BintangPancahaya <bintang.panca007@gmail.com>
2024-10-31 09:55:51 +0700

    New Change

--- SearchNilai05.java
+++ SearchNilai05.java
@@ -1,18 +1,28 @@
+import java.util.Scanner;
 public class SearchNilai05 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
+        Scanner input05 = new Scanner(System.in);
         int key = 90;
-        int hasil = 0;
+        
+        System.out.print("Masukkan banyaknya nilai mahasiswa yang akan diinput: ");
+        int mhs = input05.nextInt();
+        int nilai[] = new int[mhs];
 
-        for (int i=0;i<arrNilai.length;i++) {
-            if (key == arrNilai[i]) {
-                hasil = i;
+        for (int i=0;i<mhs;i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+": ");
+            nilai[i] = input05.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin di cari: ");
+        key = input05.nextInt();
+
+        for (int i=0;i<mhs;i++) {
+            if (key == nilai[i]) {
+                System.out.print("Nilai "+nilai[i]+" ketemu, merupakan nilai mahasiswa ke-"+(i+1));
+                break;
+            } else if (key != nilai[i] && i==(mhs-1)) {
+                System.out.print("Nilai tidak ketemu! coba lagi");
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
-        System.out.println();
-    }
-    
+    }   
 }
